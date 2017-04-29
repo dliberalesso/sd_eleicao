fbb1ffc0e31d3f29e5d983d48daa2d353fe0685f
Douglas Liberalesso <dliberalesso@gmail.com>
2017-04-28 21:52:59 -0300

    Implementa ProcessoRing. Move tudo para um arquivo.

--- eleicao.rb
+++ eleicao.rb
@@ -1,5 +1,87 @@
-require_relative 'processo_bully'
-require_relative 'processo_ring'
+class Processo
+  def initialize(n)
+    @lista = (1..n).to_a
+  end
+end
+
+class ProcessoBully < Processo
+  def detecta_crash(pid)
+    raise ArgumentError, "PID maior que a quantidade de processos criados!" if pid > @lista.size
+    raise ArgumentError, "PID deve ser maior que 0!" if pid <= 0
+
+    $coordenador = nil
+    while $coordenador.nil? do
+      if pid == @lista.last
+        $coordenador = pid
+      else
+        maiores = @lista.select {|processo| processo > pid}
+
+        maiores.each do |processo|
+          puts "#{pid} -> #{processo}: ELECTION\n"
+        end
+
+        maiores.each do |processo|
+          puts "#{processo} -> #{pid}: OK\n"
+        end
+
+        puts "---------------\n"
+
+        pid += 1
+      end
+    end
+
+    @lista.pop
+    @lista.each do |processo|
+      puts "#{$coordenador} -> #{processo}: COORDENADOR\n"
+    end
+  end
+end
+
+class ProcessoRing < Processo
+  def detecta_crash(pid)
+    raise ArgumentError, "PID maior que a quantidade de processos criados!" if pid > @lista.size
+    raise ArgumentError, "PID deve ser maior que 0!" if pid <= 0
+
+    $i = pid
+    while true
+      if @lista[$i].nil?
+        proximo = @lista[0]
+        processo = $i
+        $i = 0
+      else
+        proximo = @lista[$i]
+        processo = $i
+      end
+
+      puts "#{processo} -> #{proximo}: ELECTION(#{pid})\n"
+
+      if pid == proximo
+        break
+      elsif pid < proximo
+        pid = proximo
+      end
+      $i += 1
+    end
+
+    $i = pid
+    while true
+      if @lista[$i].nil?
+        proximo = @lista[0]
+        processo = $i
+        $i = 0
+      else
+        proximo = @lista[$i]
+        processo = $i
+      end
+
+      puts "#{processo} -> #{proximo}: COORDENADOR(#{pid})\n"
+
+      break if pid == proximo
+
+      $i += 1
+    end
+  end
+end
 
 puts "Informe o numero de processo:\n"
 n = gets.chomp.to_i
--- processo.rb
+++
@@ -1,5 +0,0 @@
-class Processo
-  def initialize(n)
-    @lista = (1..n).to_a
-  end
-end
